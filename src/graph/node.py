d01be0085da8a0c43303b3c6bc96d31ecbc96a1d
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2024-12-30 18:11:46 +0000

    Update node to include fuel, vehicles and supplies

--- src/graph/node.py
+++ src/graph/node.py
@@ -1,16 +1,27 @@
+from catastrophe import Catastrophe
+
+
 class Node:
-    # TODO import Catastrophe
-    # def __init__(self, name: str, catastrophe: Catastrophe = None):
-    def __init__(self, name: str, catastrophe=None):
+    def __init__(self,
+                 name:        str,
+                 fuel:        int         = 0,
+                 catastrophe: Catastrophe = None,
+                 vehicles:    list        = [],
+                 supplies:    dict        = {}):
         self.name = name
+        self.fuel = fuel
         self.catastrophe = catastrophe
-        # self.fuel = 0  # TODO
+        self.vehicles = vehicles
+        self.supplies = supplies
 
     def __str__(self):
         return (
             "{"
-            f"name: {self.name}"
-            f"{', catastrophe: ' + str(self.catastrophe) if self.catastrophe is not None else ''}"
+            f"  name: {self.name},\n"
+            f"  fuel: {self.fuel},\n"
+            f"  catastrophe: {self.catastrophe},\n"
+            f"  vehicles: {self.vehicles},\n"
+            f"  supplies: {self.supplies}\n"
             "}"
         )
 
@@ -22,3 +33,15 @@ class Node:
 
     def __hash__(self):
         return hash(self.name)
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "fuel": self.fuel,
+            "catastrophe": self.catastrophe.serialize() if self.catastrophe is not None else None,
+            "vehicles": [vehicle.serialize() for vehicle in self.vehicles],
+            "supplies": {
+                supply_kind: supply.serialize()
+                for supply_kind, supply in self.supplies.items()
+            }
+        }
