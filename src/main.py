d5837f033749f066bda181b5fc83ee9656febb60
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2024-12-30 18:15:52 +0000

    Update UI

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from graph import data
-import argparse
+import simulation_data
+import argparse         # command line arguments
+import json             # pretty printing
 
 
 def input_option() -> int:
@@ -14,24 +15,24 @@ def input_option() -> int:
     return option
 
 
-def display_change_graph_menu(graph_option) -> None:
+def display_change_simulation_menu(simulation_option) -> None:
     print(
         "\n"
-        f"Currently selected: {graph_option}\n"
+        f"Currently selected: {simulation_option}\n"
         "Change to:\n"
-        "1 - 1st Graph\n"
-        "2 - 2nd Graph\n"
-        "3 - 3rd Graph\n"
+        "1 - 1st Simulation\n"  # TODO add small simulation description
+        "2 - 2nd Simulation\n"  # TODO add small simulation description
+        "3 - 3rd Simulation\n"  # TODO add small simulation description
         "0 - Back"
     )
 
 
-def display_view_menu() -> None:
+def display_view_graph_menu() -> None:
     print(
         "\n"
         "View menu:\n"
-        "1 - Draw graph (graphviz)\n"
-        "2 - Draw graph (matplotlib)\n"
+        "1 - Draw graph with graphviz\n"
+        "2 - Draw graph with matplotlib\n"
         "3 - Print graph\n"
         "4 - Print graph nodes\n"
         "5 - Print graph edges\n"
@@ -69,40 +70,39 @@ def display_main_menu(verbose) -> None:
     print(
         "\n"
         "Main menu:\n"
-        "1 - Change graph\n"
-        "2 - View menu\n"
-        "3 - Search menu\n"
-        "4 - View vehicles, catastrophes and supplies\n"
+        "1 - Change simulation\n"
+        "2 - View graph menu\n"
+        "3 - View catastrophes, fleet and supplies\n"
+        "4 - Search menu\n"
         "9 - " + ("Disable" if verbose else "Enable") + " verbose mode\n"
         "0 - Exit"
     )
 
 
-def change_graph_menu(graph, graph_option) -> None:
+def change_simulation_menu(mission_planner, simulation_option) -> None:
     while True:
-        display_change_graph_menu(graph_option)
+        display_change_simulation_menu(simulation_option)
         option = input_option()
         match option:
             case 0:
-                return graph, graph_option
+                return mission_planner, simulation_option
             case 1:
-                graph_option = 1
+                simulation_option = 1
                 break
             case 2:
-                graph_option = 2
+                simulation_option = 2
                 break
             case 3:
-                graph_option = 3
+                simulation_option = 3
                 break
             case _:
                 print("Invalid option")
-    graph = data.init_graph(graph_option)
-    return graph, graph_option
+    return simulation_data.init_simulation(simulation_option), simulation_option
 
 
-def view_menu(graph) -> None:
+def view_graph_menu(graph) -> None:
     while True:
-        display_view_menu()
+        display_view_graph_menu()
         option = input_option()
         match option:
             case 0:
@@ -112,11 +112,11 @@ def view_menu(graph) -> None:
             case 2:
                 graph.draw_matplotlib()
             case 3:
-                print(graph)
+                print(graph, end="")
             case 4:
-                print(graph.nodes)
+                print(json.dumps(graph.serialize_nodes(), indent=2))
             case 5:
-                print(graph.edges)
+                graph.print_edges()
             case _:
                 print("Invalid option")
     pass
@@ -131,23 +131,23 @@ def search_menu(graph, heuristic_option, verbose) -> None:
                 break
             case 1:
                 # TODO
-                # search.bfs(graph, verbose)
+                # mission_planner.bfs(verbose)
                 pass
             case 2:
                 # TODO
-                # search.dfs(graph, verbose)
+                # mission_planner.dfs(verbose)
                 pass
             case 3:
                 # TODO
-                # search.ucs(graph, verbose)
+                # mission_planner.ucs(verbose)
                 pass
             case 4:
                 # TODO
-                # search.greedy(graph, verbose)
+                # mission_planner.greedy(verbose)
                 pass
             case 5:
                 # TODO
-                # search.a_star(graph, verbose)
+                # mission_planner.a_star(verbose)
                 pass
             case 9:
                 change_heuristic_menu(heuristic_option, graph)
@@ -178,9 +178,9 @@ def change_heuristic_menu(heuristic_option, graph) -> None:
 
 
 def main(verbose) -> None:
-    graph_option = 1
-    heuristic_option = 1
-    graph = data.init_graph(graph_option)
+    heuristic_option  = 1
+    simulation_option = 1
+    mission_planner   = simulation_data.init_simulation(simulation_option)
 
     while True:
         display_main_menu(verbose)
@@ -189,17 +189,21 @@ def main(verbose) -> None:
             case 0:
                 break
             case 1:
-                graph, graph_option = change_graph_menu(graph, graph_option)
+                mission_planner, simulation_option = \
+                    change_simulation_menu(mission_planner, simulation_option)
             case 2:
-                view_menu(graph)
+                view_graph_menu(mission_planner.graph)
             case 3:
-                search_menu(graph, heuristic_option, verbose)
+                print("Catastrophes:")
+                print(json.dumps(mission_planner.serialize_catastrophes(), indent=2))
+
+                print("\nFleet:")
+                print(json.dumps(mission_planner.serialize_fleet(), indent=2))
+
+                print("\nSupplies:")
+                print(json.dumps(mission_planner.serialize_supplies(), indent=2))
             case 4:
-                # TODO
-                # print(?.vehicles)
-                # print(?.catastrophes)
-                # print(?.supplies)
-                pass
+                search_menu(mission_planner, heuristic_option, verbose)
             case 9:
                 verbose = not verbose
                 print("Verbose mode " + ("enabled" if verbose else "disabled"))
@@ -208,8 +212,8 @@ def main(verbose) -> None:
 
 
 if __name__ == "__main__":
-    # Parse command line arguments
-    arg_parser = argparse.ArgumentParser(description="")  # TODO add description
+    # Parse command line verbose argument
+    arg_parser = argparse.ArgumentParser(description="Mission Planner")
     arg_parser.add_argument("-v", "--verbose",
                             help="Enable verbose mode",
                             action="store_true")
