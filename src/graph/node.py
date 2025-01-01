5df0e693d68528ed74bec80694e498d403c13e41
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2025-01-01 23:27:17 +0000

    Add less than operator

--- src/graph/node.py
+++ src/graph/node.py
@@ -31,6 +31,9 @@ class Node:
     def __eq__(self, other):
         return self.name == other.name
 
+    def __lt__(self, other):
+        return self.name < other.name
+
     def __hash__(self):
         return hash(self.name)
 
