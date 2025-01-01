7cb6f71487dd9c090bb8347cf21a8e7c6b2a1a84
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2025-01-01 23:28:30 +0000

    Add heuristic function 1

--- src/graph/heurisitics.py
+++ src/graph/heurisitics.py
@@ -1,3 +1,81 @@
+# Heuristic format for a node A, where B and C are nodes with catastrophes:
+"""
+"A": {
+    "B": {
+        "motorcycle": 50,
+        "car": 100,
+        "truck": 200,
+        (...)
+    },
+    "F": {
+        "motorcycle": 100,
+        "car": 200,
+        "truck": infinity,
+        (...)
+    }
+}
+"""
+
+
+# Simple heuristic:
+# Distance between the node and each catastrophe for each vehicle.
+def heuristic_fn1(params: dict) -> None:
+    # Get the parameters
+    graph        = params['graph']
+    catastrophes = params['catastrophes']
+    vehicles     = params['vehicles']
+
+    # Update heuristic values for each node in the graph
+    for node in graph.nodes:
+        heuristic_value = {}
+
+        for catastrophe_node, catastrophe in catastrophes.items():
+            heuristic_value[catastrophe_node] = {}
+
+            for vehicle in vehicles:
+                distance = graph.get_distance(node, catastrophe_node, vehicle)
+                heuristic_value[catastrophe_node][vehicle.category] = distance
+
+        graph.h[node.name] = heuristic_value
+
+
+# Medium heuristic:
 # TODO
-# OPTION calculate heuristics
-# EXAMPLE sum the distances from all catastrophes
+def heuristic_fn2(params: dict) -> None:
+    # Get the parameters
+    graph        = params['graph']
+    catastrophes = params['catastrophes']
+    vehicles     = params['vehicles']
+
+    for node in graph.nodes:
+        heuristic_value = {}
+
+        for catastrophe_node, catastrophe in catastrophes.items():
+            heuristic_value[catastrophe_node] = {}
+
+            for vehicle in vehicles:
+                # distance = graph.get_distance(node, catastrophe_node, vehicle)
+                heuristic_value[catastrophe_node][vehicle.category] = 0
+
+        graph.h[node.name] = heuristic_value
+
+
+# Complex heuristic:
+# TODO
+def heuristic_fn3(params: dict) -> None:
+    # Get the parameters
+    graph        = params['graph']
+    catastrophes = params['catastrophes']
+    vehicles     = params['vehicles']
+
+    for node in graph.nodes:
+        heuristic_value = {}
+
+        for catastrophe_node, catastrophe in catastrophes.items():
+            heuristic_value[catastrophe_node] = {}
+
+            for vehicle in vehicles:
+                # distance = graph.get_distance(node, catastrophe_node, vehicle)
+                heuristic_value[catastrophe_node][vehicle.category] = 0
+
+        graph.h[node.name] = heuristic_value
