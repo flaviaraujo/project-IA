53522da8f8c4627f90ddf57003d5ab504a01f763
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2025-01-03 01:52:23 +0000

    Add time to operation

--- src/operation.py
+++ src/operation.py
@@ -3,13 +3,18 @@
 # node           : str  - Node name where the operation takes place
 # supplies       : dict - A dictionary with keys as supply types and values as Supply objects
 
+import copy
+
+
 class Operation:
     def __init__(self,
+                 time:           int,
                  operation_type: str,
                  vehicle:        str  = None,
                  node:           str  = None,
                  fuel:           int  = None,
-                 supplies:       dict = None):
+                 supplies:       dict = None,
+                 fuel_consumed:  int  = None):
         valid_operations = {"start", "move", "refuel", "drop", "load"}
 
         if operation_type not in valid_operations:
@@ -18,27 +23,31 @@ class Operation:
                 Must be one of {valid_operations}."
             )
 
-        # TODO add start time and possibly end time
+        self.time = time
         self.operation_type = operation_type
         self.vehicle = vehicle
         self.node = node
         self.fuel = fuel
+        self.fuel_consumed = fuel_consumed
         self.supplies = supplies or {}
 
     def __str__(self):
         match self.operation_type:
             case "start":
-                return f"Vehicle {self.vehicle}: Start at node {self.node}"
+                return f"[{self.time}] Vehicle {self.vehicle}: Start at node {self.node}"
             case "move":
-                return f"Vehicle {self.vehicle}: Move to node {self.node}"
+                return f"[{self.time}] Vehicle {self.vehicle}: Move to node {self.node}"
             case "refuel":
-                return f"Vehicle {self.vehicle}: Refuel {self.fuel} liters at node {self.node}"
+                return f"[{self.time}] Vehicle {self.vehicle}: Refuel {self.fuel} liters at node {self.node}"
             case "drop":
-                return f"Vehicle {self.vehicle}: Drop supplies {self.supplies} at node {self.node}"
+                return f"[{self.time}] Vehicle {self.vehicle}: Drop supplies {self.supplies} at node {self.node}"
             case "load":
-                return f"Vehicle {self.vehicle}: Load supplies {self.supplies} at node {self.node}"
+                return f"[{self.time}] Vehicle {self.vehicle}: Load supplies {self.supplies} at node {self.node}"
             case _:
                 return "Unknown operation"
 
     def __repr__(self):
         return self.__str__()
+
+    def copy(self):
+        return copy.deepcopy(self)
