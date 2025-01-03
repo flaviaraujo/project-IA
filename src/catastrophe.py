3c1daf0f80593107018d258dc5d63c31388f222f
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2025-01-03 13:56:13 +0000

    Remove accessible_vehicles attribute, find_acessible_vehicles and calculate_travel_time_and_fuel functions

--- src/catastrophe.py
+++ src/catastrophe.py
@@ -1,11 +1,8 @@
 # The class Catastrophe is used to store the information of a catastrophe such as:
 # - time: sensitive time of response
 # - supplies_demand: dictionary with the supplies type and the amount needed
-# - accessible_vehicles: list of vehicles that can respond to the catastrophe
 
 from supply import Supply
-from heapq import heappush, heappop
-from vehicle import Vehicle
 
 import copy
 
@@ -14,7 +11,6 @@ class Catastrophe:
     def __init__(self, time, supplies_demand):
         self.time = time
         self.supplies_demand = supplies_demand
-        self.accessible_vehicles = []  # List of tuples (vehicle name, path, fuel consumption)
 
     def __str__(self):
         return (
@@ -86,15 +82,7 @@ class Catastrophe:
                 cargo_supplied[supply_kind] = provided
 
         return cargo_supplied, remaining_cargo
-    
-    def find_acessible_vehicles(self, graph, catastrophe_node, vehicles):
-        self.accessible_vehicles = []
 
-        for vehicle in vehicles:
-            travel_time, fuel_used, path = calculate_travel_time_and_fuel(graph, vehicle.current_node, catastrophe_node, vehicle)
-            if travel_time <= self.time:
-                self.accessible_vehicles.append((vehicle.name, path, fuel_used))
-                
     def assign_vehicle_to_catastrophe(self):
         # Sort accessible vehicles by fuel consumption
         self.accessible_vehicles.sort(key=lambda v: v[2])
@@ -104,48 +92,3 @@ class Catastrophe:
             best_vehicle_name, path, fuel_consumption = self.accessible_vehicles[0]
             if best_vehicle_name:
                 best_vehicle_name.assign_objective(self)
-    
-    
-                
-
-def calculate_travel_time_and_fuel(graph, start_node, end_node, vehicle):
-    if isinstance(start_node, str):
-        start_node = next((n for n in graph.nodes if n.name == start_node), None)
-
-    if isinstance(end_node, str):
-        end_node = next((n for n in graph.nodes if n.name == end_node), None)
-
-    if start_node not in graph.nodes or end_node not in graph.nodes:
-        return None, None, None
-
-    # Priority queue for Dijkstra's algorithm
-    priority_queue = [(0, 0, start_node, [])]  # (current_distance, current_fuel, current_node, path)
-    distances = {node: float('inf') for node in graph.nodes}
-    distances[start_node] = 0
-
-    visited = set()
-
-    while priority_queue:
-        current_distance, current_fuel, current_node, path = heappop(priority_queue)
-
-        if current_node in visited:
-            continue
-
-        visited.add(current_node)
-        path = path + [current_node.name]
-
-        if current_node == end_node:
-            return current_distance, current_fuel, path
-
-        for neighbor, (distance, speed_mult, travel_method, access_level) in graph.graph[current_node]:
-            if not vehicle.is_travel_possible(travel_method, access_level):
-                continue
-
-            new_distance = current_distance + distance / vehicle.speed
-            new_fuel = current_fuel + vehicle.calculate_fuel_needed(distance)
-
-            if new_distance < distances[neighbor]:
-                distances[neighbor] = new_distance
-                heappush(priority_queue, (new_distance, new_fuel, neighbor, path))
-
-    return float('inf'), float('inf'), []
